332022f8f412a0336e5a2cc8366a8cf095907e89
ElKlaus <ELKlsaus>
2016-03-03 08:56:24 +0400

    with_slider01

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $( ".sidebar-slider__block-destiny" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: 60,
+      slide: function( event, ui ) {
+        //$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    //$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    //  " - $" + $( "#slider-range" ).slider( "values", 1 ) );	
+});
