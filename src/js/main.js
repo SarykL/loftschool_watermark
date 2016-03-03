c53908dd0fd125d03013ce80c0235f102f9005af
SarykL <saryk.lav@gmail.com>
2016-03-03 09:47:21 +0200

    fix after merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,1 @@
-$(document).ready(function(){
-    $( ".sidebar-slider__block-destiny" ).slider({
-      range: "min",
-      value: 37,
-      min: 1,
-      max: 700,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.value );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
-});
+// partials/_opacity.js
