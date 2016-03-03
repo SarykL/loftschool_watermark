487e51994962ea79f867b0f7f785bd89bc217e78
ElKlaus <ELKlsaus>
2016-03-03 09:07:47 +0400

    with_slider02

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
     $( ".sidebar-slider__block-destiny" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: 60,
+      range: "min",
+      value: 37,
+      min: 1,
+      max: 700,
       slide: function( event, ui ) {
-        //$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#amount" ).val( "$" + ui.value );
       }
     });
-    //$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    //  " - $" + $( "#slider-range" ).slider( "values", 1 ) );	
+    $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
 });
