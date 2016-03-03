75ce001a9632d41c9c2e51d38b9d4dce5b3ba580
SarykL <saryk.lav@gmail.com>
2016-03-03 23:15:55 +0200

    fix js partials

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
 //= partials/_opacity.js
 
-// creates uploaded file name appearing in input
-       $('input[type="file"]').on('change', function () {
-           var file = ($(this)).val();
-           var fileName = file.split('\\');
-           fileName = fileName[fileName.length - 1];
-           $(this)
-           .parent()
-           .siblings('.fake__input')
-           .find('.fake__placeholder')
-           .text(fileName);
-       });
+//= partials/_upload.js
