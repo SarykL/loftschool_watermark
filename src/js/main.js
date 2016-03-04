168695a1c4d953cac896e6c90ae5ea49400305ba
skarakash <karakash.sergey@gmail.com>
2016-03-04 13:26:05 +0200

    added pseudo element bottom to the inputs block

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
 // creates uploaded file name appearing in input
-       $('input[type="file"]').on('change', function () {
-           var file = ($(this)).val();
-           var fileName = file.split('\\');
-           fileName = fileName[fileName.length - 1];
-           $(this)
-           .parent()
-           .siblings('.fake__input')
-           .find('.fake__placeholder')
-           .text(fileName);
-       });
+$('input[type="file"]').on('change', function () {
+   var file = ($(this)).val();
+   var fileName = file.split('\\');
+   var fileType = ('.' + file.split('.')[1]);
+   var fileTypes = ['.jpeg', '.png', '.jpg', '.bmp', '.tiff'];
+
+   if (($.inArray(fileType, fileTypes)) == -1) {
+     console.log('stop');
+     file = 'Загрузите изображение';
+     return false;
+   } else {
+     console.log('ok');
+   }
+   fileName = fileName[fileName.length - 1];
+   $(this)
+   .parent()
+   .siblings('.fake__input')
+   .find('.fake__placeholder')
+   .text(fileName);
+
+
+});
+
+// $('input[type="file"]').on('change', function () {
+//
+// });
