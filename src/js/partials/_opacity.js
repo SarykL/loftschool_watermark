5538a1429d7d0a4c6c4df1d1375fdfc059322b34
ElKlaus <ELKlsaus>
2016-03-05 23:02:49 +0400

    opacity04

--- src/js/partials/_opacity.js
+++ src/js/partials/_opacity.js
@@ -1,12 +1,33 @@
 ;$(document).ready(function(){
     $( ".sidebar-slider__block-destiny" ).slider({
       range: "min",
-      value: 37,
-      min: 1,
-      max: 700,
+      value: 0.3,
+      min: 0,
+      max: 1,
+      step: 0.01,
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.value );
       }
+
+    
+     
     });
     $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
+    
+    //Opacity
+    $(".sidebar-slider__block-destiny").slider({
+    slide: function(event, ui) {
+      $('.sidebar-position-content').css({ opacity : ui }); 
+        console.log(ui);
+    }
+    })
+
+    /*$(".sidebar-position-square").css({
+      opacity: 'value1'
+    });
+
+
+    $('#ToTop').css({ opacity : 1 }); 
+    */
 });
+
