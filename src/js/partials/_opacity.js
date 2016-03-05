2be8dbe31c6218601c9b41107ddb283d90a3c92a
SarykL <saryk.lav@gmail.com>
2016-03-06 01:31:22 +0200

    change test class

--- src/js/partials/_opacity.js
+++ src/js/partials/_opacity.js
@@ -9,11 +9,11 @@
       step: 0.01,
       create: function(event, ui) {
         def_value = $(this).slider( "values", 0 );
-        $('.sidebar-position-content').css({ opacity : def_value });
+        $('.workspace__square').css({ opacity : def_value });
       },
       slide: function(event, ui) {
         op_value = $(this).slider( "values", 0 );
-        $('.sidebar-position-content').css({ opacity : op_value });
+        $('.workspace__square').css({ opacity : op_value });
       }
     });
     $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
