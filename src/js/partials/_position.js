13e4d8be7ff760e2108bbf45e6fc5e49c6ba2a4e
Sergey <sera.prokop@gmail.com>
2016-03-06 14:22:57 +0300

    Position in input

--- src/js/partials/_position.js
+++ src/js/partials/_position.js
@@ -1,10 +1,16 @@
-$('.sidebar-position__link').on('click', function(e){
-  e.preventDefault();
-  var aTTr = $(this).attr('data-position');
-  $('.workspace__square').position({
-    my: aTTr,
-    at: aTTr,
-    of: '.workspace__unit',
-    collision: 'none'
+$(document).ready(function(){
+  $('.sidebar-position__link').on('click', function(e){
+    e.preventDefault();
+    var aTTr = $(this).attr('data-position');
+    $('.workspace__square').position({
+      my: aTTr,
+      at: aTTr,
+      of: '.workspace__unit',
+      collision: 'none',
+    });
+    var positionX = $('.workspace__square').position().left;
+    var positionY = $('.workspace__square').position().top;
+    $('.coordinateX').val(positionX);
+    $('.coordinateY').val(positionY);
   });
-})
+});
