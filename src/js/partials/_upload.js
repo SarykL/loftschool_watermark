44cee3ed9d9c10790df37f26c0700178d1fafa55
ElKlaus <ELKlsaus>
2016-03-09 05:32:39 +0400

    with-img

--- src/js/partials/_upload.js
+++ src/js/partials/_upload.js
@@ -1,5 +1,5 @@
 // creates uploaded file name appearing in input
- $('input[type="file"]').on('change', function () {
+/*$('input[type="file"]').on('change', function () {
 	var file = ($(this)).val();
 	var fileName = file.split('\\');
 	fileName = fileName[fileName.length - 1];
@@ -8,4 +8,24 @@
 	.siblings('.fake__input')
 	.find('.fake__placeholder')
 	.text(fileName);
- });
+ });*/
+
+ ;$(document).ready(function(){
+	$(function () {
+	    'use strict';
+	    // Initialize the jQuery File Upload widget:
+	    $('#fileupload').fileupload({
+	        // Uncomment the following to send cross-domain cookies:
+	        //xhrFields: {withCredentials: true},
+	        url: 'server/php/',
+	        add: function(e, data) {
+	        	console.log('add');
+	        	data.submit();
+	        },
+	        done: function(e, data) {
+	        	console.log('done');
+	        }	        
+
+	    });
+	});
+});
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,1 +1,4 @@
 //= libs/jquery-ui.min.js
+//= libs/jquery.ui.widget.js
+//= libs/jquery.iframe-transport.js
+//= libs/jquery.fileupload.js
