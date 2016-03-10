adb2da76363758e3ed30dcec7d1a75ae8428593f
SarykL <saryk.lav@gmail.com>
2016-03-10 10:12:41 +0200

    uglify code

--- src/js/partials/_social-sharing.js
+++ src/js/partials/_social-sharing.js
@@ -9,24 +9,24 @@
 		title = document.title,
 		location = window.location.href;
 
-		fb.on('click', function () {
-			var url = 'https://www.facebook.com/sharer/sharer.php?u=';
-			url += encodeURIComponent(location);
-			window.open(url, '','width=' + sharePopupWidth + ',height=' + sharePopupHeight);
-		});
+	fb.on('click', function () {
+		var url = 'https://www.facebook.com/sharer/sharer.php?u=';
+		url += encodeURIComponent(location);
+		window.open(url, '','width=' + sharePopupWidth + ',height=' + sharePopupHeight);
+	});
 
-		tw.on('click', function () {
-			var  url = 'http://twitter.com/share?';
-			url += 'text=' + title;
-			url += '&url=' + location;
-			window.open(url, '','width=' + sharePopupWidth + ',height=' + sharePopupHeight);
-		});
-		vk.on('click', function () {
-			var  url = 'https://vk.com/share.php?';
-			url += '&title=' + title;
-			url += 'url=' + location;
-			url += '&noparse=true';
-			window.open(url, '','width=' + sharePopupWidth + ',height=' + sharePopupHeight);
-		});
+	tw.on('click', function () {
+		var  url = 'http://twitter.com/share?';
+		url += 'text=' + title;
+		url += '&url=' + location;
+		window.open(url, '','width=' + sharePopupWidth + ',height=' + sharePopupHeight);
+	});
+	vk.on('click', function () {
+		var  url = 'https://vk.com/share.php?';
+		url += '&title=' + title;
+		url += 'url=' + location;
+		url += '&noparse=true';
+		window.open(url, '','width=' + sharePopupWidth + ',height=' + sharePopupHeight);
+	});
 
 });
