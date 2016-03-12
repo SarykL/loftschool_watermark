10ca9cfcc075580dd812f8683dd1c24506d7b216
SarykL <saryk.lav@gmail.com>
2016-03-13 01:28:40 +0200

    width inspection

--- src/js/partials/_upload.js
+++ src/js/partials/_upload.js
@@ -1,6 +1,8 @@
 // creates uploaded file name appearing in input
 /**/
 var dataSize = {
+	canvasWidth: 650,
+	canvasHeight: 530,
 	bgWidth: 0,
 	bgHeight: 0,
 	wmHeight: 0,
@@ -11,48 +13,56 @@ var dataSize = {
 };
 
 $( 'input[type="file"]' )
-  .on( 'change', function() {
-    var file = ( $( this ) )
-      .val();
-    var fileName = file.split( '\\' );
-    fileName = fileName[ fileName.length - 1 ];
-    $( this )
-      .parent()
-      .siblings( '.fake__input' )
-      .find( '.fake__placeholder' )
-      .text( fileName );
-  } );
+	.on( 'change', function() {
+		var file = ( $( this ) )
+			.val();
+		var fileName = file.split( '\\' );
+		fileName = fileName[ fileName.length - 1 ];
+		$( this )
+			.parent()
+			.siblings( '.fake__input' )
+			.find( '.fake__placeholder' )
+			.text( fileName );
+	} );
 
 var uploads = [ '#fileupload', '#watermark' ],
-    spaces = ['.workspace__background','.workspace__watermark'];
+	spaces = ['.workspace__background','.workspace__watermark'];
 $.each( uploads, function( index, item ) {
 	if ( item == '#fileupload' ) {
-	    $(item).fileupload({
-	      dataType: 'json',
-	      url: 'server/php/',
-	      type: 'POST',
-	      add: function( e, data ) {
-	        data.submit();
-	      },
-	      done: function( e, data ) {
-	        var ground = data.result.files[ 0 ],
-	        	img = $('<img></img>').css({
-	        		position: 'static'
-	        	});
-	    	img.attr('src', ground.url);
-	    	img.load(function(){
-                   console.log(img.width() + ':' + img.height());
-                   dataSize.bgWidth = img.width();
-                   dataSize.bgHeight = img.height();
-                   console.log(dataSize);
+		$(item).fileupload({
+			dataType: 'json',
+			url: 'server/php/',
+			type: 'POST',
+			add: function( e, data ) {
+				data.submit();
+			},
+			done: function( e, data ) {
+				var ground = data.result.files[ 0 ],
+					img = $('<img></img>').css({
+						position: 'static'
+					});
+			img.attr('src', ground.url);
+			img.load(function(){
+				console.log(img.width() + ':' + img.height());
+				dataSize.bgWidth = img.width();
+				dataSize.bgHeight = img.height();
+				console.log(dataSize);
+				if (dataSize.bgWidth >= dataSize.canvasWidth & dataSize.bgWidth > dataSize.bgHeight) {
 					//  if widthbg> canvas &
-		    	img.css({
-		    		width: '100%',
-		    		height: 'auto'
-		    	});
-		    	// else
-               });
-	    	img.appendTo(spaces[0]);
+					img.css({
+						width: '100%',
+						height: 'auto'
+					});
+					
+				}
+				// if (dataSize.bgHeight >= dataSize.canvasHeight) {
+				// 	img.css({
+				// 		width: 'auto',
+				// 		height: '100%'
+				// 	});
+				// }
+			});
+				img.appendTo(spaces[0]);
 			var result = $.ajax({
 				url: 'server/php/merge.php',
 				type: 'POST',
@@ -61,63 +71,66 @@ $.each( uploads, function( index, item ) {
 			});
 			console.log(e);
 			return result;
-	      }
+				}
 
 
-	    });
+			});
 	} else if ( item == '#watermark' ) {
-	    $(item).fileupload({
-	      dataType: 'json',
-	      url: 'server/php/',
-	      add: function( e, data ) {
-	        data.submit();
-	      },
-	      done: function( e, data ) {
-	        var water = data.result.files[ 0 ],
+			$(item).fileupload({
+				dataType: 'json',
+				url: 'server/php/',
+				add: function( e, data ) {
+					data.submit();
+				},
+				done: function( e, data ) {
+					var water = data.result.files[ 0 ],
 
-	        	img = $('<img></img>');
-            // var maxHeightSp = dataSize.bgHeight - dataSize.wmHeight;
+						img = $('<img></img>');
+						// var maxHeightSp = dataSize.bgHeight - dataSize.wmHeight;
 
-          //   .css({
-	        	// 	position: 'absolute'
-	        	// });
+					//   .css({
+						// 	position: 'absolute'
+						// });
 
-	    	img.attr('src', water.url);
-		    	img.load(function(){
-           // console.log(img.width() + ':' + img.height());
-           dataSize.wmWidth = img.width();
-           dataSize.wmHeight = img.height();
-
-           //------------- Max position spinner-------------------
-
-           var heightSpinner = $('.workspace__background').height() - $('.workspace__watermark').height();
-
-            var maxY = $( '.coordinateY' ).spinner( "option", "max", heightSpinner );
-
-            var widthSpinner = $('.workspace__background').width() - $('.workspace__watermark').width();
-
-            var maxX = $( '.coordinateX' ).spinner( "option", "max", widthSpinner );
-
-            maxY.spinner();
-            maxX.spinner();
-
-            //------------- End max position spinner-------------------
+				img.attr('src', water.url);
+					img.load(function(){
+					 // console.log(img.width() + ':' + img.height());
+					 dataSize.wmWidth = img.width();
+					 dataSize.wmHeight = img.height();
 
 
 
-           // console.log(dataSize);
+
+
+					 // console.log(dataSize);
 			//  if widthbg> canvas &
 
-			// dataSize.scaleWidth = dataSize.bgWidth/dataSize.wmWidth;
-			// dataSize.scaleHeight = dataSize.bgHeight/dataSize.wmHeight;
-    	img.css({
+			dataSize.scaleWidth = dataSize.bgWidth/dataSize.wmWidth;
+			dataSize.scaleHeight = dataSize.bgHeight/dataSize.wmHeight;
+			img.css({
 			width: dataSize.wmWidth/dataSize.scaleWidth + 'px',
-	    		// height: dataSize.wmHeight/dataSize.scaleHeight + 'px'
-	    		height: 'auto'
-    	});
-    	// else
-       });
-	    	img.appendTo(spaces[1]);
+					// height: dataSize.wmHeight/dataSize.scaleHeight + 'px'
+					height: 'auto'
+			});
+			// else
+
+
+					 //------------- Max position spinner-------------------
+
+					 var heightSpinner = $('.workspace__background').height() - $('.workspace__watermark').height();
+
+						var maxY = $( '.coordinateY' ).spinner( "option", "max", heightSpinner );
+
+						var widthSpinner = $('.workspace__background').width() - $('.workspace__watermark').width();
+
+						var maxX = $( '.coordinateX' ).spinner( "option", "max", widthSpinner );
+
+						maxY.spinner();
+						maxX.spinner();
+
+						//------------- End max position spinner-------------------
+			 });
+				img.appendTo(spaces[1]);
 			$.ajax({
 				url: '../server/php/merge.php',
 				type: 'POST',
@@ -134,27 +147,27 @@ $.each( uploads, function( index, item ) {
 			.always(function() {
 				console.log("complete");
 			});
-	      }
-	    });
+				}
+			});
 	}
 });
 
-$.ajax({
-	url: '../server/php/merge.php',
-	type: 'POST',
-	dataType: 'json',
-	data: 'water',
-})
-.done(function(ans) {
-	console.log("success");
-	console.log(ans);
-})
-.fail(function() {
-	console.log("error123");
-})
-.always(function() {
-	console.log("complete123");
-});
+// $.ajax({
+// 	url: '../server/php/merge.php',
+// 	type: 'POST',
+// 	dataType: 'json',
+// 	data: 'water',
+// })
+// .done(function(ans) {
+// 	console.log("success");
+// 	console.log(ans);
+// })
+// .fail(function() {
+// 	console.log("error123");
+// })
+// .always(function() {
+// 	console.log("complete123");
+// });
 
 	/*
 	var mergephp = 'server/php/merge.php'
@@ -190,21 +203,21 @@ $.ajax({
 
 
 /*
-    $(item).fileupload({
-      dataType: 'json',
-      url: 'server/php/',
-      add: function( e, data ) {
-        console.log( 'add' );
-        data.submit();
-      },
-      done: function( e, data ) {
-        var upload = data.result.files[ 0 ],
-        	img = $('<img></img>');
-        console.log( upload );
-        console.log(current);
-        console.log(upload.url);
-    	img.attr('src', upload.url);
-    	img.appendTo(spaces[0]);
-      }
-    });
+		$(item).fileupload({
+			dataType: 'json',
+			url: 'server/php/',
+			add: function( e, data ) {
+				console.log( 'add' );
+				data.submit();
+			},
+			done: function( e, data ) {
+				var upload = data.result.files[ 0 ],
+					img = $('<img></img>');
+				console.log( upload );
+				console.log(current);
+				console.log(upload.url);
+			img.attr('src', upload.url);
+			img.appendTo(spaces[0]);
+			}
+		});
 */
