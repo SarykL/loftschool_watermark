0ebe57ceff42a2aa09c05c83e35571a3980b406b
SarykL <saryk.lav@gmail.com>
2016-03-13 17:06:21 +0200

    fix reset module

--- src/js/partials/_reset.js
+++ src/js/partials/_reset.js
@@ -2,10 +2,10 @@
 	$('.reset-btn').on('click', function (e) {
 		e.preventDefault();
 		$('.sidebar-slider__block-destiny').slider('option', 'value', 0.3);
-		$('.workspace__square').css({ opacity : 0.66666666 });
+		$('.workspace__watermark').css({ opacity : 0.66666666 });
 
-		$('.workspace__square').css({left : 0});
-		$('.workspace__square').css({top : 0});
+		$('.workspace__watermark').css({left : 0});
+		$('.workspace__watermark').css({top : 0});
 		$('.position-input').spinner('value', 0);
 		$('.sidebar-position__link').removeClass('sidebar-position__link--active');
 		$('.top-left').addClass('sidebar-position__link--active');
